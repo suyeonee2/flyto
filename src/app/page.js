3b5ecd2c6fd4cd1081a96b9f09f56d00fe5df597
suyeon <suyeonee2@gmail.com>
2025-05-31 14:17:12 +0900

    add spline

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,34 @@
+import styles from "./page.module.css";
+import Link from "next/link";
+import Spline from "@splinetool/react-spline";
+
 export default function Home() {
-  return <>Choosely</>;
+  return (
+    <div className={styles.home_Wrapper}>
+      <div className={styles.home_header}>
+        <h1 className={styles.home_logo}>choosely</h1>
+        <hr className={styles.divider} />
+      </div>
+
+      {/* 여기 hero 들어감 */}
+      <div className="hero">
+        <Spline scene="https://prod.spline.design/TRfTj83xgjIdHPmT/scene.spline" />
+      </div>
+
+      <div className={styles["button-group"]}>
+        <Link href="/project" className={styles["choosely-button"]}>
+          Project
+        </Link>
+        <Link href="/portfolio" className={styles["choosely-button"]}>
+          portfolio
+        </Link>
+        <Link href="/about" className={styles["choosely-button"]}>
+          About
+        </Link>
+        <Link href="/contact" className={styles["choosely-button"]}>
+          Contact
+        </Link>
+      </div>
+    </div>
+  );
 }
