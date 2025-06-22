c2520c44bc90517d08db020a0e79130720eb86f0
suyeon <suyeonee2@gmail.com>
2025-06-22 14:30:07 +0900

    update fwee

--- src/app/examples/fwee_voluming/page.js
+++ src/app/examples/fwee_voluming/page.js
@@ -4,8 +4,6 @@ import { useRouter } from "next/navigation";
 import styles from "./page.module.css";
 import ContentLayout from "@/Components/ContentLayout";
 import Header from "@/Components/Header/Header";
-import startImg from "./assets/startImg.png";
-import Image from "next/image";
 
 export default function FweeVolumingStart() {
   const router = useRouter();
@@ -15,14 +13,15 @@ export default function FweeVolumingStart() {
       <Header />
       <ContentLayout className={styles.bgFwee}>
         <div className={styles.startSection}>
-          <Image
-            src={startImg}
-            alt="fwee 시작 이미지"
-            className={styles.thumbnail}
-            width={891}
-            height={1260}
-            unoptimized
+          <video
+            src="/fwee.mp4"
+            autoPlay
+            muted
+            loop
+            playsInline
+            className={styles.videoBackground}
           />
+
           <button
             className={styles.startButton}
             onClick={() => router.push("/examples/fwee_voluming/question")}
@@ -30,6 +29,7 @@ export default function FweeVolumingStart() {
             테스트 시작하기
           </button>
         </div>
+
         <p>
           * 포트폴리오 목적으로 제작된 콘텐츠이며, 실제 브랜드와 무관합니다.
         </p>
