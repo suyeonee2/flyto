37b1313b24e14888d5eaa384bd207d11751352b0
suyeon <suyeonee2@gmail.com>
2025-06-29 20:17:22 +0900

    test

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,48 +1,48 @@
-"use client";
+// app/layout.js
 
 import "./globals.css";
 import Script from "next/script";
 import { META } from "@/components/utils/metadata";
 
+export const metadata = {
+  title: META.title,
+  description: META.description,
+  keywords: META.keyword,
+  openGraph: {
+    title: META.openGraph.title,
+    description: META.openGraph.description,
+    url: META.url,
+    siteName: META.siteName,
+    images: [
+      {
+        url: META.ogImage,
+        width: 800,
+        height: 400,
+        alt: META.openGraph.title,
+      },
+    ],
+    type: "website",
+    locale: "ko_KR",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: META.openGraph.title,
+    description: META.openGraph.description,
+    images: [META.ogImage],
+  },
+  verification: {
+    google: META.googleVerification,
+    other: {
+      "naver-site-verification": META.naverVerification,
+    },
+  },
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="ko">
-      <head>
-        <title>{META.title}</title>
-
-        {/* 필수 메타태그 */}
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="robots" content="index, follow" />
-
-        {/* 아이콘 */}
-        <link rel="icon" href={META.icon} />
-        <link rel="apple-touch-icon" href={META.appleIcon} />
-
-        {/* SEO */}
-        <meta name="description" content={META.description} />
-        <meta name="keywords" content={META.keyword.join(", ")} />
-
-        {/* Open Graph */}
-        <meta property="og:title" content={META.openGraph.title} />
-        <meta property="og:description" content={META.openGraph.description} />
-        <meta property="og:image" content={META.ogImage} />
-        <meta property="og:url" content={META.url} />
-        <meta property="og:site_name" content={META.siteName} />
-        <meta property="og:type" content="website" />
-        <meta property="og:locale" content="ko_KR" />
-
-        {/* Twitter */}
-        <meta name="twitter:title" content={META.openGraph.title} />
-        <meta name="twitter:description" content={META.openGraph.description} />
-        <meta name="twitter:image" content={META.ogImage} />
-
-        {/* 사이트 소유자 인증 */}
-        <meta
-          name="google-site-verification"
-          content={META.googleVerification}
-        />
-        <meta name="naver-site-verification" content={META.naverVerification} />
+      <body suppressHydrationWarning>
+        {children}
 
         {/* GA 스크립트 */}
         {META.googleAnalyticsId && (
@@ -63,8 +63,7 @@ export default function RootLayout({ children }) {
             </Script>
           </>
         )}
-      </head>
-      <body suppressHydrationWarning>{children}</body>
+      </body>
     </html>
   );
 }
